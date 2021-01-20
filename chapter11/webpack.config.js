e717655dcd3fb50300abffce0fe455f4c2c023d6
weifeng.zhang <wulakaka@live.com>
2021-01-20 09:30:32 +0800

    add chapter 11 file

---
+++ chapter11/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: path.resolve(__dirname, './js/main.js')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Chapter 11'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist')
+  }
+}
